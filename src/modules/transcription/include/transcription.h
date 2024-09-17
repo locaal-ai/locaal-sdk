d709066777d6c1c13341fc06e20efe2b0e997b2e
Roy Shilkrot <roy.shil@gmail.com>
2024-09-17 17:53:35 -0400

    Refactor build script for Linux and MacOS in CI workflow

--- src/modules/transcription/include/transcription.h
+++ src/modules/transcription/include/transcription.h
@@ -11,37 +11,36 @@ struct TranscriptionResult {
 	uint64_t start_timestamp_ms;
 	uint64_t end_timestamp_ms;
 	std::string language;
-    bool is_partial;
+	bool is_partial;
 };
 
 class Transcription {
 public:
-    Transcription();
-    ~Transcription();
+	Transcription();
+	~Transcription();
 
-    void setTranscriptionParams(const std::string& language);
+	void setTranscriptionParams(const std::string &language);
 
-    void setModelDownloadCallbacks(
-        std::function<void(const std::string&, const std::string&)> onSuccess,
-        std::function<void(const std::string&, const std::string&)> onFailure,
-        std::function<void(const std::string&, const std::string&)> onProgress
-    );
+	void setModelDownloadCallbacks(
+		std::function<void(const std::string &, const std::string &)> onSuccess,
+		std::function<void(const std::string &, const std::string &)> onFailure,
+		std::function<void(const std::string &, const std::string &)> onProgress);
 
-    void setTranscriptionCallback(std::function<void(const TranscriptionResult&)> callback);
+	void setTranscriptionCallback(std::function<void(const TranscriptionResult &)> callback);
 
-    void startTranscription();
-    void stopTranscription();  // Added for completeness
+	void startTranscription();
+	void stopTranscription(); // Added for completeness
 
-    void processAudio(const std::vector<float>& audioData);
+	void processAudio(const std::vector<float> &audioData);
 
 private:
-    std::string language_;
-    std::function<void(const std::string&, const std::string&)> onModelDownloadSuccess_;
-    std::function<void(const std::string&, const std::string&)> onModelDownloadFailure_;
-    std::function<void(const std::string&, const std::string&)> onModelDownloadProgress_;
-    std::function<void(const TranscriptionResult&)> transcriptionCallback_;
+	std::string language_;
+	std::function<void(const std::string &, const std::string &)> onModelDownloadSuccess_;
+	std::function<void(const std::string &, const std::string &)> onModelDownloadFailure_;
+	std::function<void(const std::string &, const std::string &)> onModelDownloadProgress_;
+	std::function<void(const TranscriptionResult &)> transcriptionCallback_;
 
-    // Add any other necessary private members
+	// Add any other necessary private members
 };
 
 } // namespace locaal
